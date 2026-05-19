a3a62dd2343670954354e56f9eb10bd2892d5632
Runar K. Halse <runar.halse@apparat.no>
2026-05-19 13:40:56 +0200

    ## - update for new release v0.2.2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "BagIdSDK",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v16)
     ],
     products: [
         .library(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BagIdSDK",
-            url: "https://bagidpublic.blob.core.windows.net/ios-sdk/BagIdSDK-v0.0.1.xcframework.zip",
-            checksum: "1c527759c466f207cd2ce354cfa1ae374b08b2af54313e792a962ac897f3a989"
+            url: "https://bagidpublic.blob.core.windows.net/ios-sdk/BagIdSDK-v0.2.2.xcframework.zip",
+            checksum: "4fe23376d4124463bb3499ed5ad7ee0c47532e429d8cccbd85c76e555a3e32fe"
         )
     ]
 )
